1341ad855d96a657566f2e667162ede7184a604f
lucaslzl <lznladeira@gmail.com>
2020-06-19 16:10:19 -0300

    Added configurations.

---
+++ configurations/create_configs.py
@@ -0,0 +1,72 @@
+import json
+
+import numpy as np
+
+
+def write_json(p:dict, variable, folder):
+    with open(f'{folder}/{variable}.json', 'w') as fp:
+        json.dump(p, fp, indent=4)
+
+
+# Pandemic A
+pandemic_a = {
+    	"isolamento": 0.0,
+    	"chance_infeccao": 0.3,
+    	"tempo_infectado": 70,
+    	"distancia_infectar": 100,
+    	"random_infected": 0.1,
+        "stationary_infection": [1800, 3600, 5400]
+    }
+
+count = 1
+# Change isolation
+for i in np.arange(0.1, 1.0, 0.1):
+    i = "{:.1f}".format(float(i))
+    p = pandemic_a.copy()
+    p['isolamento'] = float(i)
+    write_json(p, f'isolation_{i}', 'pandemic_a')
+
+# Change infection chance
+for j in np.arange(0.1, 1.0, 0.1):
+    j = "{:.1f}".format(float(j))
+    p = pandemic_a.copy()
+    p['change_infeccao'] = float(j)
+    write_json(p, f'infeccao_{j}', 'pandemic_a')
+
+# Change infection time
+for k in np.arange(50, 150, 10):
+    p = pandemic_a.copy()
+    p['tempo_infectado'] = float(k)
+    write_json(p, f'tempo_{k}', 'pandemic_a')
+
+
+# Pandemic B
+# pandemic_b = {
+#     	"isolamento": 0.0,
+#     	"chance_infeccao": 0.3,
+#     	"tempo_infectado": 70,
+#     	"distancia_infectar": 100,
+#     	"random_infected": 0.1,
+#         "stationary_infection": [1800, 3600, 5400]
+#     }
+#
+# count = 1
+# # Change isolation
+# for i in np.arange(0.1, 1.0, 0.1):
+#     i = "{:.1f}".format(float(i))
+#     p = pandemic_a.copy()
+#     p['isolamento'] = float(i)
+#     write_json(p, f'isolation_{i}', 'pandemic_a')
+#
+# # Change infection chance
+# for j in np.arange(0.1, 1.0, 0.1):
+#     j = "{:.1f}".format(float(j))
+#     p = pandemic_a.copy()
+#     p['change_infeccao'] = float(j)
+#     write_json(p, f'infeccao_{j}', 'pandemic_a')
+#
+# # Change infection time
+# for k in np.arange(50, 150, 10):
+#     p = pandemic_a.copy()
+#     p['tempo_infectado'] = float(k)
+#     write_json(p, f'tempo_{k}', 'pandemic_a')
